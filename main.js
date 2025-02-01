7f070d3d5a4aa012e775dab3ef51610bf5811877
Slava <avvaken7@gmail.com>
2025-02-01 18:30:04 +0200

    add a burger menu

--- main.js
+++ main.js
@@ -8,3 +8,22 @@ const swiper = new Swiper(".swiper", {
         clickable: true
     }
 });
+
+// BurgerMenu
+
+const mobileBtn = document.querySelector('.mobile-btn');
+const mobileMenu = document.querySelector('.mobile-nav-menu');
+const noscroll = document.querySelector('body');
+
+mobileBtn.addEventListener('click', () => {
+    mobileBtn.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+    noscroll.classList.toggle('overflow-hidden');
+})
+
+document.querySelectorAll('.mobile-nav-item').forEach(n => n.
+    addEventListener('click', () => {
+        mobileBtn.classList.remove('active');
+        mobileMenu.classList.remove('active');
+        noscroll.classList.remove('overflow-hidden');
+}));
